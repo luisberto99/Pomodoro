89790d46abd7cbd2f5d52cfc79ea117f78132f71
Alberto Medina <lamedina@unah.hn>
2021-09-11 18:54:37 -0600

    Listado de tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
+import { ToDoListsComponent } from './to-do-lists/to-do-lists.component';
+
+import {TableModule} from 'primeng/table';
+import { FormsModule } from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+import {ChipsModule} from 'primeng/chips';
+import {ButtonModule} from 'primeng/button';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {ToastModule} from 'primeng/toast';
+import {MessageService} from 'primeng/api';
+import {ConfirmPopupModule} from 'primeng/confirmpopup';
+import {ConfirmationService} from 'primeng/api';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimerComponent
+    TimerComponent,
+    ToDoListsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    TableModule,
+    InputTextModule,
+    ChipsModule,
+    ButtonModule,
+    MessagesModule,
+    MessageModule,
+    InputNumberModule,
+    ToastModule,
+    ConfirmPopupModule,
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    ConfirmationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/to-do-lists/to-do-lists.component.ts
@@ -0,0 +1,170 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import {ConfirmationService} from 'primeng/api';
+import Dexie from "dexie";
+
+@Component({
+  selector: 'app-to-do-lists',
+  templateUrl: './to-do-lists.component.html',
+  styleUrls: ['./to-do-lists.component.scss'],
+})
+export class ToDoListsComponent implements OnInit {
+
+  db: any;
+  rows: Tarea[] = [];
+
+
+  tareas: any = [];
+
+  tarea = '';
+  categoria = [];
+  cat = [];
+  tareaActive = 0;
+
+  constructor(private messageService: MessageService,
+              private confirmationService: ConfirmationService) {
+    let a = JSON.parse(localStorage.getItem('tareas') + '');
+    if (a) {
+      this.tareas = a;
+    }
+  }
+
+  ngOnInit(): void {
+    this.makeDatabase();
+    this.connectToDatabase();
+    this.loadData()
+    
+  }
+
+  makeDatabase(): void {
+    this.db = new Dexie('Pomodoro');
+    this.db.version(1).stores({
+      Tareas: '++id, categoria, descripcion, pomodorosEstimados, pomodoros, horaInicio'
+    });
+    this.loadRows();
+  }
+
+  connectToDatabase(): void {
+    this.db.open().catch((error:any) => {
+      alert("Errod during connecting to database : " + error);
+    }).finally((e:any) => {
+      console.log(e);
+      this.loadRows();
+    });
+  }
+
+  loadRows(): void {
+    this.db.Tareas.toArray((p:any) => this.rows = p);
+    console.log(this.rows);
+  }
+
+  addRow(): void {
+    this.db.Tareas.add({
+      categoria: [this.cat + ''],
+      descripcion: this.tarea,
+      pomodorosEstimados: 1,
+      pomodoros: 0,
+      horaInicio: new Date(),
+    }).then((e:any) => {
+      console.log(e);
+      console.log("ADD");
+      this.loadRows();
+    });
+  }
+
+  agregar() {
+    this.addRow();
+    if (this.tarea != '') {
+      let t = {
+        categoria: [this.cat + ''],
+        descripcion: this.tarea,
+        pomodorosEstimados: 1,
+        pomodoros: 0,
+        horaInicio: new Date(),
+      };
+      this.tareas.unshift(t);
+      localStorage.setItem('tareas', JSON.stringify(this.tareas));
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Tarea agregada',
+        detail: this.tarea,
+      });
+      (this.tarea = ''), (this.categoria = []);
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Agregar titulo de la tarea'
+      });
+    }
+  }
+
+
+  
+  loadData(){
+   let n = 0;
+
+   let x = setInterval( () => {
+      this.db.Tareas.reverse().toArray((p:any) => this.tareas = p)
+      console.log(this.tareas);
+      n++;
+      if(n == 3){
+        clearInterval(x)
+      }
+    },1);
+  }
+
+  play(id:number){
+    if(this.tareaActive != id){
+      this.tareaActive = id
+      console.log(id);
+    }else{
+      this.tareaActive = 0;
+    }
+  }
+
+  plusPomodoro(id:number, pomodoros:number){
+    console.log(id);
+    pomodoros += 1;
+    this.db.Tareas.update(id, {"pomodorosEstimados": pomodoros})
+    this.loadData()
+  }
+
+  delete(id:number, event: Event){
+    this.confirmationService.confirm({
+      target: event.target,
+      message: "¿Esta seguro de eliminar la tarea?",
+      icon: 'pi pi-explamation-triangle',
+      accept: () => {
+        this.db.Tareas.delete(id).then(()=>{
+          this.messageService.add({
+            severity: 'warn',
+            summary: 'Tarea eliminada'
+          });
+          this.loadData()
+        }).catch((e:any) =>{
+          console.log(e);
+          
+        });
+        
+      },
+      reject: (e:any) => {
+        console.log(e);
+        console.log("error");
+        
+      }
+    })
+    console.log(id);
+    
+  }
+
+
+}
+
+interface Tarea {
+  id?: number,
+  categoria: String,
+  descripcion: String,
+  pomodorosEstimados: number,
+  pomodoros: number,
+  horaInicio: Date,
+}
